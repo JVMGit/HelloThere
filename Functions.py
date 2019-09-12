867d078a4cf8b9ba633e9d8e5ed7863ae00f78f1
JVMGit <jvmestudios@gmail.com>
2019-09-12 16:29:28 +0200

    Force functions added

---
+++ Functions.py
@@ -0,0 +1,12 @@
+def greetings():
+    print('OH hello there')
+
+def your_name():
+    name=input('Tu nombre: ')
+    return name
+
+def ObiSad(name):
+    print(name,'you were supposed to bring balance to the Force')
+    input()
+
+
--- Hello.py
+++ Hello.py
@@ -1,3 +1,5 @@
-print('OH hello there')
-a=input()
-print(a,'you were supposed to bring balance to the Force')
+from Functions import greetings, your_name, ObiSad
+name = 'ala, un fallo'
+greetings()
+name = your_name()
+ObiSad(name)
